525e1370826d75669881a16f576d2da58ab9f7b2
Jocelyne Li <jocelyneli0622@gmail.com>
2025-11-14 23:46:00 -0500

    hide on scroll header

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthProvider } from '../contexts/AuthContext';
 import { LanguageProvider } from '../contexts/LanguageContext';
 import Header from './Header';
 import Footer from './Footer';
 
 export default function Layout({ children }) {
+  const [hideHeader, setHideHeader] = useState(false);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      const current = window.scrollY;
+
+      if (current > lastScrollY && current > 80) {
+        // scrolling down
+        setHideHeader(true);
+      } else {
+        // scrolling up
+        setHideHeader(false);
+      }
+
+      lastScrollY = current;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <LanguageProvider>
       <AuthProvider>
-        <div className="min-h-screen flex flex-col bg-background text-foreground">
-          <Header />
-          <main className="flex-1">
-            {children}
-          </main>
+        <div className="min-h-screen flex flex-col bg-gradient-to-br from-purple-50 via-white to-indigo-50">
+          <Header hide={hideHeader} />
+          <main className="flex-1">{children}</main>
           <Footer />
         </div>
       </AuthProvider>
     </LanguageProvider>
   );
 }
-
